8357d5342df6d3a4e84f827ae609f1493d36001d
Nicolas Wagner <contact@wagner-nicolas.com>
2017-07-13 13:47:37 +0200

    add change color navbar scroll

--- js/scrollSpy.js
+++ js/scrollSpy.js
@@ -1,4 +1,5 @@
-$('body').scrollspy({ target: '#navbar-top' })
+
+$('body').scrollspy({ target: '#navbar-top' });
 $("#navbar-top ul li a[href^='#']").on('click', function(e) {
  // prevent default anchor click behavior
  e.preventDefault();
@@ -11,5 +12,10 @@ $("#navbar-top ul li a[href^='#']").on('click', function(e) {
      // when done, add hash to url
      // (default click behaviour)
      window.location.hash = hash;
-   });
+  });
+});
+$(document).scroll(function () {
+  console.log('scroll');
+  var $nav = $(".navbar-fixed-top");
+  $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
 });
