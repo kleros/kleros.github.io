b0842768b4d3a7124c1085c92591676d94936db8
Nicolas Wagner <contact@wagner-nicolas.com>
2017-07-18 14:30:04 +0200

    style section protocol

--- js/scrollSpy.js
+++ js/scrollSpy.js
@@ -15,6 +15,7 @@ $("#navbar-top ul li a[href^='#']").on('click', function(e) {
   });
 });
 $(document).scroll(function () {
+  console.log('scroll');
   var $nav = $(".navbar-fixed-top");
   $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
 });
